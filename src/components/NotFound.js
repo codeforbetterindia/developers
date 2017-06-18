6010db3bd5da126293b35f729f31a1f45d523f27
Himank Barve <hbarve1592@gmail.com>
2017-06-18 20:46:55 +0530

    Not Found component is updated.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,45 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const NotFound = () => <div>Not Found</div>;
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import Card, { CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
 
-export default NotFound;
+import AppLayout from './AppLayout';
+
+const styleSheet = createStyleSheet('NotFound', {
+  container: {
+    margin: '10px',
+    minHeight: '80vh',
+    backgroundImage: 'url("/public/photos/not-found.png")',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundOrigin: 'content-box',
+    backgroundSize: 'cover',
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
+
+const NotFound = ({ classes }) => {
+  return (
+    <AppLayout>
+      <Paper className={classes.container}>
+        <Typography type="display3" gutterBottom>
+          Error: 404
+        </Typography>
+        <Typography type="body1" gutterBottom>
+          Page Not Found!
+        </Typography>
+      </Paper>
+    </AppLayout>
+  );
+};
+
+export default withStyles(styleSheet)(NotFound);
